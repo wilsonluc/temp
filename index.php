289c2b51c498eb1efb1243304618242b8d33b79e
dumpgitter <79299826+dumpgitter@users.noreply.github.com>
2021-02-19 16:54:54 +1100

    Init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assignment 2</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css">
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+    <script src='../wireframe.js'></script>
+</head>
+
+<body>
+
+<header>
+    <!-- Image sourced from www.rmit.edu.au for educational purposes only -->
+    <div><img src='../../media/RMIT-Logo.png' alt='RMIT University logo' />Wilson Luc</div>
+</header>
+
+<nav>
+    <div><a href="home.html">ANZAC Douglas Raymond Baker - Letters Home</a></div>
+</nav>
+
+<main>
+    <article id='Website Under Construction'>
+        <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+        <img src='../../media/website-under-construction.png' alt='Website Under Construction'/>
+    </article>
+</main>
+
+
+</body>
+</html>
---
+++ post-validation.php
@@ -0,0 +1,10 @@
+<?php
+
+$firstname = '';
+$firstnameError = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Put your POST data processing and validation code here
+}
+
+?>
